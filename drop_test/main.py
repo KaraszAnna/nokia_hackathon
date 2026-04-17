ef947a65bf62111352b42d71c5bcb2af9d4985ae
Kárász Anna <karaszanna16@gmail.com>
2026-04-17 15:28:28 +0200

    drop_test_1.0

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 
+def min_num_of_drops(devs, max_height):
+    reach = [0] * (devs + 1)
+    tries = 0
+    
+    while reach[devs] < max_height:
+        tries += 1
+        for i in range(devs, 0, -1):
+            reach[i] = reach[i] + reach[i-1] + 1
+            
+    return tries
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
